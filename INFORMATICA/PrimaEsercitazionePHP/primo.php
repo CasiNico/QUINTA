2b7ddfb00c8534c8482b8ed354ea7299a8bf2135
C@5! <nicocasiraghi2007@gmail.com>
2025-10-09 10:53:00 +0200

    Lezione091025-GestioneInput&Array

--- INFORMATICA/PrimaEsercitazionePHP/primo.php
+++ INFORMATICA/PrimaEsercitazionePHP/primo.php
@@ -1,4 +1,17 @@
 <?php
     // codice php
-    echo("<h1>Ciao Mondo!</h1>");
+    $nome = "Nicolò";
+    $s = "<h1 style='color: red'>Ciao $nome </h1>"; 
+    // tra apici doppi le variabili vengono interpretate
+    // tra apici singoli no
+    // devo mettere il punto per concatenare le stringhe
+    echo($s);
+
+    // gestione array
+
+    $lista = array(1,2,3,4,5,6,7,8,9,0);
+    foreach($lista as $k => $v){
+        echo ("<p> $k: $v </p>");
+    }
+
 ?>
