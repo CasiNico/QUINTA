855637c4bbb5a4fbc923f4593292ca1ccdb8bcaf
C@5! <nicocasiraghi2007@gmail.com>
2025-11-07 14:07:42 +0100

    info es_cookie 071125

---
+++ INFORMATICA/es_cookie/salva.php
@@ -0,0 +1,20 @@
+<?php
+
+$login = $_GET['login'];
+$pws = $_GET['pws'];
+
+if ($_COOKIE[$login]) {
+    echo 'utente loggato';
+}else {
+    echo 'utente in registrazione';
+    setcookie($login, $pws);
+    echo 'utente registrato con successo';
+}
+
+echo("
+<a href='index.html'>
+    <button>TORNA A INDEX.HTML</button>
+</a>
+");
+
+?>
