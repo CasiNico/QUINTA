7ac18c94a4bbd8d58b1fe69dddbb4abda8196950
C@5! <nicocasiraghi2007@gmail.com>
2026-02-05 10:55:32 +0100

    update es_biblioteca 050226 info

--- INFORMATICA/SQL/es_biblioteca 050226.sql
+++ INFORMATICA/SQL/es_biblioteca 050226.sql
@@ -27,5 +27,26 @@ create table biblioteca.prestiti (
     data_restituzione date not null,
     constraint fk_prestiti_utente foreign key (id_utente) references utente(id_utente),
     constraint fk_prestiti_libro foreign key (id_libro) references libro(id_libro)
-)
+);
 
+insert into biblioteca.utente (id_utente, nome, cognome, email, eta)
+values (11, 'nicolò', 'casiraghi', 'nc2@gmail.com', 18);
+
+insert into biblioteca.libro (id_libro, titolo, autore, isbn, anno_pubblicazione)
+values (12, 'I fratelli Karamazov', 'Fedor Dostoevsky', 'A123B2', '1880-11-01');
+
+insert into biblioteca.prestiti (id_prestito, id_utente, id_libro, data_prestito, data_restituzione) 
+values (132, '11', '12', '2026-02-05', '2026-03-05');
+
+insert into biblioteca.utente values (6, 'nico2', 'casi2', 'nc3@prova.it', 13);
+/*
+non funziona, da errore xché eta <14
+*/
+
+insert into biblioteca.utente values (6, 'nico2', 'casi2', 'nc3@prova.it', 14);
+
+insert into biblioteca.libro (id_libro, titolo, autore, isbn, anno_pubblicazione)
+values (13, 'Delitto e Castigo', 'Fedor Dostoevsky', 'A123B2', '1499-11-01');
+
+insert into biblioteca.prestiti (id_prestito, id_utente, id_libro, data_prestito, data_restituzione) 
+values (132, '11', '12', '2026-02-05', '2026-03-05');
