4b2b48a3ab427b37cbf3c8f86624b2cd0770b245
C@5! <nicocasiraghi2007@gmail.com>
2026-02-12 09:19:27 +0100

    update es 300126 tpsi

--- TPSI/es300126/server.js
+++ TPSI/es300126/server.js
@@ -4,9 +4,19 @@ const PORT = 1533
 const HOSTNAME = "localhost"
 
 const server = http.createServer((req, res) => {
+    
     if(req.url === "/"){
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain")
         res.end("ciao");
     }
+
+    if(req.url === "/numbers" && req.method === "GET"){
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json")
+        res.end(JSON.stringify({ numbers: number}));
+    }
+
 })
 
 server.listen(PORT, HOSTNAME, () => {
