2bd733218c06d6b8868a36db1e8b36d09a4d0b27
C@5! <nicocasiraghi2007@gmail.com>
2026-04-24 16:06:14 +0200

    update 240426 tpsi

--- TPSI/ExpressEisley/endpoints_da_aggiungere.js
+++ TPSI/ExpressEisley/endpoints_da_aggiungere.js
@@ -1,86 +1,69 @@
-// E1
+const app = require('express')
 
-app.use('/bevande', (req, res, next) => {
-  const header = req.headers['x-gradazione-max'];
-  const parsed = parseInt(header);
-  req.gradazioneMax = (!header || isNaN(parsed)) ? null : parsed;
-  next();
-});
+let server = app();
 
-app.get('/bevande', (req, res) => {
-  if (req.gradazioneMax === null) {
-    return res.json(bevande);
-  }
 
-  const risultato = bevande.filter(b => b.gradazione <= req.gradazioneMax);
-  res.json(risultato); 
-});
 
-// E2
+let clienti = [
+    { id: 1, nome: 'Han Solo', specie: 'umano', credito: 1500 },
+    { id: 2, nome: 'Greedo', specie: 'rodiano', credito: 300 },
+    { id: 3, nome: 'Chewbacca', specie: 'wookiee', credito: 900 },
+    { id: 4, nome: 'Hammerhead', specie: 'ithoriano', credito: 200 }
+];
 
-app.use('/clienti', (req, res, next) => {
-  req.ruolo = req.headers['x-ruolo'] || 'ospite';
-  next();
-});
 
-app.get('/clienti/:id/ordini', (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ errore: 'ID non valido' });
-  }
+//https://starwars.fandom.com/wiki/Spotchka
+let bevande = [
+    { id: 1, nome: 'Corellian Ale', prezzo: 50, gradazione: 8 },
+    { id: 2, nome: 'Juri Juice', prezzo: 80, gradazione: 15 },
+    { id: 3, nome: 'Spotchka', prezzo: 120, gradazione: 20 },
+    { id: 4, nome: 'Merenzane Gold', prezzo: 200, gradazione: 5 }
+];
 
-  const cliente = clienti.find(c => c.id === id);
-  if (!cliente) {
-    return res.status(404).json({ errore: 'Cliente non trovato' });
-  }
+let ordini = [];
 
-  const ordiniCliente = ordini.filter(o => o.cliente === cliente.nome);
+let taglie = [];
 
-  if (req.ruolo === 'admin') {
-    return res.json(ordiniCliente);
-  }
-
-  const ordiniSenzaCosto = ordiniCliente.map(o => {
-    const { costo_totale, ...resto } = o;
-    return resto;
-  });
-  res.json(ordiniSenzaCosto);
-});
-
-// E3
-
-app.get('/clienti/:id/riepilogo', (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ errore: 'ID non valido' });
-  }
-
-  const cliente = clienti.find(c => c.id === id);
-  if (!cliente) {
-    return res.status(404).json({ errore: 'Cliente non trovato' });
-  }
-
-  const ordiniCliente = ordini.filter(o => o.cliente === cliente.nome);
-
-  const totale_speso = ordiniCliente.reduce((acc, o) => acc + o.costo_totale, 0);
-
-  let bevanda_preferita = null;
-  if (ordiniCliente.length > 0) {
-    const contatore = {};
-    for (const o of ordiniCliente) {
-      contatore[o.bevanda] = (contatore[o.bevanda] || 0) + o.quantita;
+let missioni = [
+    {
+        id: 1,
+        codice: 'AURORA-1',
+        descrizione: 'Recupero piani della Morte Nera',
+        pianeta: 'Scarif',
+        rischio: 'alto',
+        clearance: 3,
+        agente: 'Cassian Andor'
+    },
+    {
+        id: 2,
+        codice: 'NEBULA-4',
+        descrizione: 'Sorveglianza porto di Mos Eisley',
+        pianeta: 'Tatooine',
+        rischio: 'basso',
+        clearance: 1,
+        agente: 'Fulcrum'
+    },
+    {
+        id: 3,
+        codice: 'ECLIPSE-7',
+        descrizione: 'Sabotaggio generatori imperiali',
+        pianeta: 'Lothal',
+        rischio: 'alto',
+        clearance: 2,
+        agente: 'Hera Syndulla'
+    },
+    {
+        id: 4,
+        codice: 'PHANTOM-2',
+        descrizione: 'Estrazione agente sotto copertura',
+        pianeta: 'Coruscant',
+        rischio: 'critico',
+        clearance: 3,
+        agente: 'Sconosciuto'
     }
-    bevanda_preferita = Object.entries(contatore).sort((a, b) => b[1] - a[1])[0][0];
-  }
+];
 
-  const taglie_attive = taglie.filter(t => t.clienteId === id && t.attiva === true).length;
+// app.use()
 
-  res.json({
-    cliente: cliente.nome,
-    credito_attuale: cliente.credito,
-    numero_ordini: ordiniCliente.length,
-    totale_speso,
-    bevanda_preferita,
-    taglie_attive
-  });
-});
+
+server.listen(3000, () => {console.log("servizio aperto sulla porta 3000")});
